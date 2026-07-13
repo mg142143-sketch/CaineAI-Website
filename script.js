52883cd8019f0ec068fa1edfec87053b5fe57ed9
mg142143-sketch <mg142143@stu.hemetusd.org>
2026-07-13 04:41:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+async function sendMessage(){
 
+let box=document.getElementById("message");
+let chat=document.getElementById("chat");
+
+let message=box.value;
+
+chat.innerHTML += 
+"<p>You: "+message+"</p>";
+
+box.value="";
+
+
+let response = await fetch(
+"https://caine-ai-1.onrender.com/chat",
+{
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+message:message
+
+})
+
+});
+
+
+let data = await response.json();
+
+
+chat.innerHTML +=
+"<p>Caine: "+data.reply+"</p>";
+
+
+let voice = new SpeechSynthesisUtterance(data.reply);
+
+speechSynthesis.speak(voice);
+
+
+}
